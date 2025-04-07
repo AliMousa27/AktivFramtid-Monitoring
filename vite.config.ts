68043c9d8a07f1398f905b4d1c5be06fba4ab7db
AliMousa27 <jafar.myhero@gmail.com>
2025-04-07 13:44:59 +0200

    change repo name

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-export default defineConfig({
-  base: "/AktivFramtid-Monitoring/",
-  plugins: [react()],
-});
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/AktivFramtid-Monitoring/" : "/", // Replace <repository-name> with your GitHub repository name
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), ...(mode === "development" ? [] : [])],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
